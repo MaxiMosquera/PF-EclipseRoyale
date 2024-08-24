594689ac41d7e18aae19efe0ba91e130acce3aaa
PabloRodriguez16 <pablorodriguez6002@gmail.com>
2024-08-24 20:20:42 -0300

    correción en las fechas y precarga de imagenes

--- back/src/utils/roomsimages.ts
+++ back/src/utils/roomsimages.ts
@@ -1,20 +1,14 @@
-import { Category } from "src/enum/room.enums";
+import { Category } from 'src/enum/room.enums';
 
-export const roomImages= {
+export const roomImages = {
   [Category.SUITE]: [
-    'https://assets.milestoneinternet.com/cdn-cgi/image/f=auto/highgate-hotels/nau-hotels/siteimages/nau-salgados-dunas-suites-standard-double-room-with-balcony-view.jpg?width=600&height=600'
+    'https://assets.milestoneinternet.com/cdn-cgi/image/f=auto/highgate-hotels/nau-hotels/siteimages/nau-salgados-dunas-suites-standard-double-room-with-balcony-view.jpg?width=600&height=600',
   ],
   [Category.SUITE_PREMIUM]: [
-    'https://www.athinasuites.com/images/Homepage_Resort_INDEX/deluxe-suite.jpg'
+    'https://www.athinasuites.com/images/Homepage_Resort_INDEX/deluxe-suite.jpg',
   ],
-    [Category.LOFT]: [
-      'https://www.lamasu.it/public/prodotti/378/1__40M0554.jpg'
-      
-    ],
-    [Category.LOFT_PREMIUM]: [
-      'https://stock.adobe.com/es/images/modern-luxury-beach-loft-apartment-with-sea-view/42365903',
-      'https://stock.adobe.com/es/images/modern-luxury-beach-loft-apartment-with-sea-view/42523787',
-      'https://stock.adobe.com/es/images/modern-luxury-beach-loft-apartment-with-sea-view/43093607',
-      
-    ],
-  };
+  [Category.LOFT]: ['https://www.lamasu.it/public/prodotti/378/1__40M0554.jpg'],
+  [Category.LOFT_PREMIUM]: [
+    'https://stock.adobe.com/es/images/modern-luxury-beach-loft-apartment-with-sea-view/42365903',
+  ],
+};
