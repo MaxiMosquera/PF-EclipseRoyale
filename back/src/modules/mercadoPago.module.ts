f5e2670d7099deebba8188dd174e977f8b33e75b
PabloRodriguez16 <pablorodriguez6002@gmail.com>
2024-08-28 17:45:30 -0300

    listo

--- back/src/modules/mercadoPago.module.ts
+++ back/src/modules/mercadoPago.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MercadoPagoController } from 'src/controllers/mercadoPago.controller';
-import { ReservationRepository } from 'src/repositories/reservation.repository';
 import { MercadoPagoService } from 'src/services/mercadoPago.service';
 import { ReservationModule } from './reservation.module';
 
 @Module({
   imports: [ReservationModule],
   controllers: [MercadoPagoController],
-  providers: [MercadoPagoService, ReservationRepository],
+  providers: [MercadoPagoService],
 })
 export class MercadopagoModule {}
