80e2a2595596fdd831c35973a2affdf788edb9c0
PabloRodriguez16 <pablorodriguez6002@gmail.com>
2024-09-10 11:41:57 -0300

    mejora en mail de reserva

--- back/src/modules/mercadoPago.module.ts
+++ back/src/modules/mercadoPago.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { MercadoPagoController } from 'src/controllers/mercadoPago.controller';
 import { MercadoPagoService } from 'src/services/mercadoPago.service';
 import { ReservationModule } from './reservation.module';
+import { MailService } from 'src/services/mail.service';
 
 @Module({
   imports: [ReservationModule],
   controllers: [MercadoPagoController],
-  providers: [MercadoPagoService],
+  providers: [MercadoPagoService, MailService],
 })
 export class MercadopagoModule {}
--- back/src/services/mail.service.ts
+++ back/src/services/mail.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import * as nodemailer from 'nodemailer';
+import { Reservation } from 'src/entities/reservation.entity';
 import { User } from 'src/entities/user.entity';
 
 @Injectable()
@@ -7,7 +8,6 @@ export class MailService {
   private transporter: nodemailer.Transporter;
 
   constructor() {
-    // Configura el transportador con las variables de entorno
     this.transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
@@ -15,28 +15,27 @@ export class MailService {
         pass: process.env.SMTP_PASS,
       },
       tls: {
-        rejectUnauthorized: false, // Evitar que los certificados inválidos causen errores
+        rejectUnauthorized: false,
       },
     });
   }
 
   async prueba(): Promise<void> {
     const mailOptions = {
-      from: 'maximiliano.mosquera1@gmail.com', // Remitente
-      to: 'indisardi99@gmail.com', // Destinatario
-      subject: 'Prueba', // Asunto
+      from: 'maximiliano.mosquera1@gmail.com',
+      to: 'indisardi99@gmail.com',
+      subject: 'Prueba',
 
-      html: 'Prueba', // Texto en HTML
+      html: 'Prueba',
     };
   }
-  // Método para enviar el correo de confirmación
 
   async sendUserConfirmation(user: User): Promise<void> {
     const mailOptions = {
-      from: '"Hotel Eclipse Royale" <contactoeclipseroyale@gmail.com>', // Remitente
-      to: user.email, // Destinatario
-      subject: 'Registro exitoso', // Asunto
-      text: '¡Gracias por registrarte!', // Texto en plano
+      from: '"Hotel Eclipse Royale" <contactoeclipseroyale@gmail.com>',
+      to: user.email,
+      subject: 'Registro exitoso',
+      text: '¡Gracias por registrarte!',
       html: `<!DOCTYPE html>
   <html lang="es">
   <head>
@@ -127,31 +126,42 @@ export class MailService {
       </div>
     </div>
   </body>
-  </html>`, // HTML del cuerpo del correo
+  </html>`,
     };
 
     await this.transporter.sendMail(mailOptions);
   }
 
-  async sendReservationemail(email: string, name: string): Promise<void> {
+  async sendReservationEmail(
+    email: string,
+    name: string,
+    reservation: Reservation,
+    services: string[],
+  ): Promise<void> {
     console.log('Sending reservation email to:', email);
 
     const mailOptions = {
-      from: '"Hotel Eclipse Royale" <contactoeclipseroyale@gmail.com>', // Remitente
-      to: email, // Destinatario
-      subject: 'Reserva exitosa', // Asunto
-      text: '¡Gracias por reservar!', // Texto en plano
+      from: '"Hotel Eclipse Royale" <contactoeclipseroyale@gmail.com>',
+      to: email,
+      subject: 'Detalles de tu reserva en Hotel Eclipse Royale',
+      text: '¡Gracias por tu reserva!',
       html: `
-    <body style="font-family: Arial, sans-serif; background-color: #f0f4ff; margin: 0; padding: 20px; display: flex; justify-content: center; align-items: center; min-height: 100vh; box-sizing: border-box;">
-      <div style="background-color: #ffffff; max-width: 600px; width: 100%; padding: 20px; border-radius: 10px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); text-align: center;">
-        <img style="margin-bottom: 20px; border-radius: 8px;" src="https://front-hotel-app-six.vercel.app//_next/image?url=%2Flogo.png&w=256&q=75" alt="Logo" width="150" height="100">
-        <h1 style="color: #333333; font-size: 24px;">Reserva exitosa, ${name}!</h1>
-        <p style="font-style: italic; color: #333333;"><strong>contactoeclipseroyale@gmail.com</strong></p>
-        <p style="color: #333333;">Tu registro en Hotel Eclipse Royale se ha realizado con éxito, te invitamos a explorar nuestros servicios en el siguiente enlace:</p>
-        <a href="https://front-hotel-app-six.vercel.app/ style="display: inline-block; margin-top: 20px; padding: 10px 20px; background-color: #7f69b9; color: white; text-decoration: none; border-radius: 5px; font-size: 16px;">Ir a Home</a>
-        <p style="color: #666666; margin-top: 20px;">Si no puedes hacer click en el botón, visita este link <a href="https://front-hotel-app-six.vercel.app/ style="color: #5120cc; text-decoration: none;">siguiente enlace</a></p>
-      </div>
-    </body>`, // HTML del cuerpo del correo
+      <body style="font-family: Arial, sans-serif; background-color: #f0f4ff; margin: 0; padding: 20px; display: flex; justify-content: center; align-items: center; min-height: 100vh; box-sizing: border-box;">
+        <div style="background-color: #ffffff; max-width: 600px; width: 100%; padding: 20px; border-radius: 10px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); text-align: center;">
+          <img style="margin-bottom: 20px; border-radius: 8px;" src="https://front-hotel-app-six.vercel.app//_next/image?url=%2Flogo.png&w=256&q=75" alt="Logo" width="150" height="100">
+          <h1 style="color: #333333; font-size: 20px;">¡Tu reserva ha sido confirmada, ${name}!</h1>
+          <p style="color: #333333;">Aquí tienes los detalles de tu reserva en Hotel Eclipse Royale:</p>
+          <p style="color: #333333;"><strong>ID de Reserva:</strong> ${reservation.id}</p>
+          <p style="color: #ff3838; font-size: 10px"><strong>Puedes usar el ID de la reserva para cancelar tu reserva por medio de nuestro chatbot</strong></p>          
+          <p style="color: #333333;"><strong>Checkin:</strong> ${reservation.startDate}</p>
+          <p style="color: #333333;"><strong>Checkout:</strong> ${reservation.endDate}</p>
+          <p style="color: #333333;"><strong>Servicios solicitados:</strong> ${services.join(', ') || 'No se solicitaron servicios adicionales'}</p>
+          <p style="color: #333333;"><strong>Número de Habitación:</strong> ${reservation.room.number}</p>
+          <p style="color: #333333;"><strong>Costo Total:</strong> $${reservation.price}</p>
+          <a href="https://front-hotel-app-six.vercel.app/" style="display: inline-block; margin-top: 20px; padding: 10px 20px; background-color: #7f69b9; color: white; text-decoration: none; border-radius: 5px; font-size: 16px;">Volver al hotel</a>
+          <p style="color: #666666; margin-top: 20px;">Si no puedes hacer click en el botón, visita este <a href="https://front-hotel-app-six.vercel.app/" style="color: #5120cc; text-decoration: none;">enlace</a>.</p>
+        </div>
+      </body>`,
     };
 
     await this.transporter.sendMail(mailOptions);
@@ -159,10 +169,10 @@ export class MailService {
 
   async sendUserConfirmationGoogle(user: User): Promise<void> {
     const mailOptions = {
-      from: '"Hotel Eclipse Royale" <contactoeclipseroyale@gmail.com>', // Remitente
-      to: user.email, // Destinatario
-      subject: 'Registro exitoso', // Asunto
-      text: '¡Gracias por registrarte!', // Texto en plano
+      from: '"Hotel Eclipse Royale" <contactoeclipseroyale@gmail.com>',
+      to: user.email,
+      subject: 'Registro exitoso',
+      text: '¡Gracias por registrarte!',
       html: `<html lang="es">
   <head>
     <meta charset="UTF-8">
@@ -252,7 +262,7 @@ export class MailService {
       </div>
     </div>
   </body>
-  </html>`, // HTML del cuerpo del correo
+  </html>`,
     };
 
     await this.transporter.sendMail(mailOptions);
@@ -260,10 +270,10 @@ export class MailService {
 
   async sendAdminWelcome(user: User, password: string): Promise<void> {
     const mailOptions = {
-      from: '"Hotel Eclipse Royale" <contactoeclipseroyale@gmail.com>', // Remitente
-      to: user.email, // Destinatario
-      subject: 'Bienvenido a Hotel Eclipse Royale', // Asunto
-      text: '¡Bienvenido al equipo!', // Texto en plano
+      from: '"Hotel Eclipse Royale" <contactoeclipseroyale@gmail.com>',
+      to: user.email,
+      subject: 'Bienvenido a Hotel Eclipse Royale',
+      text: '¡Bienvenido al equipo!',
       html: `<!DOCTYPE html>
 <html lang="es">
   <head>
@@ -377,7 +387,7 @@ export class MailService {
     </div>
   </body>
 </html>
-`, // HTML del cuerpo del correo
+`,
     };
 
     await this.transporter.sendMail(mailOptions);
